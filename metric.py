683c82d74586c94bcec687b6e7ffe32a913674a8
lukeolson <luke.olson@gmail.com>
2017-05-10 21:05:28 -0500

    clean up metric

--- metric.py
+++ metric.py
@@ -2,13 +2,13 @@ import numpy as np
 from scipy import interpolate
 from scipy.interpolate import interp1d
 
+
 class Metric:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.compute()
-
 
     def boundary(self):
         x = self.x
@@ -22,18 +22,17 @@ class Metric:
         sp = [3*.5, .5, -4*.5]
         sm = [-3*.5, 4*.5, -.5]
 
-        x_s[:,0] = sm[0] * x[:,0] + sm[1] * x[:,1] + sm[2] * x[:,2]
-        y_s[:,0] = sm[0] * y[:,0] + sm[1] * y[:,1] + sm[2] * y[:,2]
-        x_s[:,-1] = sp[0] * x[:,-1] + sp[-1] * x[:,-2] + sp[-2] * x[:,-3]
-        y_s[:,-1] = sp[0] * y[:,-1] + sp[-1] * y[:,-2] + sp[-2] * x[:,-3]
+        x_s[:, 0] = sm[0] * x[:, 0] + sm[1] * x[:, 1] + sm[2] * x[:, 2]
+        y_s[:, 0] = sm[0] * y[:, 0] + sm[1] * y[:, 1] + sm[2] * y[:, 2]
+        x_s[:, -1] = sp[0] * x[:, -1] + sp[-1] * x[:, -2] + sp[-2] * x[:, -3]
+        y_s[:, -1] = sp[0] * y[:, -1] + sp[-1] * y[:, -2] + sp[-2] * x[:, -3]
 
-        x_t[0,:] = sm[0] * x[0,:] + sm[1] * x[1,:] + sm[2] * x[2,:]
-        y_t[0,:] = sm[0] * y[0,:] + sm[1] * y[1,:] + sm[2] * y[2,:]
-        x_t[-1,:] = sp[0] * x[-1,:] + sp[-1] * x[-2,:] + sp[-2] * x[-3,:]
-        y_t[-1,:] = sp[0] * y[-1,:] + sp[-1] * y[-2,:] + sp[-2] * y[-3,:]
+        x_t[0, :] = sm[0] * x[0, :] + sm[1] * x[1, :] + sm[2] * x[2, :]
+        y_t[0, :] = sm[0] * y[0, :] + sm[1] * y[1, :] + sm[2] * y[2, :]
+        x_t[-1, :] = sp[0] * x[-1, :] + sp[-1] * x[-2, :] + sp[-2] * x[-3, :]
+        y_t[-1, :] = sp[0] * y[-1, :] + sp[-1] * y[-2, :] + sp[-2] * y[-3, :]
 
         return (x_s, y_s, x_t, y_t)
-
 
     def interp_diff(self, axis):
         # TODO: no reason this cant be a vector function
@@ -48,20 +47,20 @@ class Metric:
                 for i in range(1, x.shape[1]-1, 2):
                     jj = j-1
                     if i < x.shape[1] - 1:
-                        f0 = interp1d(np.arange(3), x[j-1, i-1:i+2],kind='linear')
-                        f1 = interp1d(np.arange(3), x[j+1, i-1:i+2],kind='linear')
+                        f0 = interp1d(np.arange(3), x[j-1, i-1:i+2])
+                        f1 = interp1d(np.arange(3), x[j+1, i-1:i+2])
                         XHx_t[jj, i-1] = (f1(.5) - f0(.5)) * .5
                         XHx_t[jj, i] = (f1(1.5) - f0(1.5)) * .5
-                        f0 = interp1d(np.arange(3), y[j-1, i-1:i+2],kind='linear')
-                        f1 = interp1d(np.arange(3), y[j+1, i-1:i+2],kind='linear')
+                        f0 = interp1d(np.arange(3), y[j-1, i-1:i+2])
+                        f1 = interp1d(np.arange(3), y[j+1, i-1:i+2])
                         XHy_t[jj, i-1] = (f1(.5) - f0(.5)) * .5
                         XHy_t[jj, i] = (f1(1.5) - f0(1.5)) * .5
                     elif i == x.shape[1] - 1:
-                        f0 = interp1d(np.arange(3), x[j-1, i-2:i+1],kind='linear')
-                        f1 = interp1d(np.arange(3), x[j+1, i-2:i+1],kind='linear')
+                        f0 = interp1d(np.arange(3), x[j-1, i-2:i+1])
+                        f1 = interp1d(np.arange(3), x[j+1, i-2:i+1])
                         XHx_t[jj, i-1] = (f1(.5) - f0(.5)) * .5
-                        f0 = interp1d(np.arange(3), y[j-1, i-2:i+1],kind='linear')
-                        f1 = interp1d(np.arange(3), y[j+1, i-2:i+1],kind='linear')
+                        f0 = interp1d(np.arange(3), y[j-1, i-2:i+1])
+                        f1 = interp1d(np.arange(3), y[j+1, i-2:i+1])
                         XHy_t[jj, i-1] = (f1(.5) - f0(.5)) * .5
 
             return XHx_t, XHy_t
@@ -90,25 +89,24 @@ class Metric:
 
             return YHx_s, YHy_s
 
-
     def compute(self):
         x = self.x
         y = self.y
 
-        XHx_s = x[1:-1,1:] - x[1:-1,:-1]
-        XHy_s = y[1:-1,1:] - y[1:-1,:-1]
-        YHx_t = x[1:,1:-1] - x[:-1,1:-1]
-        YHy_t = y[1:,1:-1] - y[:-1,1:-1]
+        XHx_s = x[1:-1, 1:] - x[1:-1, :-1]
+        XHy_s = y[1:-1, 1:] - y[1:-1, :-1]
+        YHx_t = x[1:, 1:-1] - x[:-1, 1:-1]
+        YHy_t = y[1:, 1:-1] - y[:-1, 1:-1]
 
         XHx_t, XHy_t = self.interp_diff(axis=0)
         YHx_s, YHy_s = self.interp_diff(axis=1)
 
         (Cx_s, Cy_s, Cx_t, Cy_t) = self.boundary()
 
-        Cx_t[1:-1,:] = (x[2:,:] - x[:-2,:]) * .5
-        Cy_t[1:-1,:] = (y[2:,:] - y[:-2,:]) * .5
-        Cx_s[:,1:-1] = (x[:,2:] - x[:,:-2]) * .5
-        Cy_s[:,1:-1] = (y[:,2:] - y[:,:-2]) * .5
+        Cx_t[1:-1, :] = (x[2:, :] - x[:-2, :]) * .5
+        Cy_t[1:-1, :] = (y[2:, :] - y[:-2, :]) * .5
+        Cx_s[:, 1:-1] = (x[:, 2:] - x[:, :-2]) * .5
+        Cy_s[:, 1:-1] = (y[:, 2:] - y[:, :-2]) * .5
 
         g_11 = YHx_s * YHx_s + YHy_s * YHy_s
         XHg_11 = XHx_s * XHx_s + XHy_s * XHy_s
